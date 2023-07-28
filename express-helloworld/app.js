732591d3318123338a45a4fa1a0d2ddb92530649
William Santana <williamdiegocampos@gmail.com>
2023-07-28 18:33:46 -0300

    Add /hello-dev

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/helo-dev', function (req, res) {
+  res.send('Hello Dev Mui Loko!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
